96273d8ecb80623f8dcb2963c94bfd83883c9567
mikhail1207 <mikhail1207@d2f4722b-fb4a-0410-96a1-771b68bbab1a>
2008-05-07 14:06:25 +0000

    work under scheduler  functionality

---
+++ raven-core/src/main/java/org/raven/sched/Scheduler.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2008 Mikhail Titov.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.raven.sched;
+
+/**
+ *
+ * @author Mikhail Titov
+ */
+public interface Scheduler 
+{
+    /**
+     * Adds job to the scheduler.
+     */
+    public void addJob(Job job);
+    /**
+     * Removes job from the scheduler.
+     */
+    public void removeJob(int id);
+    /**
+     * Shutdowns the scheduler.
+     */
+    public void shutdown();
+}
