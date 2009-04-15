9d7b07dc4f736d7e60175982b87f6e109bbcd796
mikhail1207 <mikhail1207@d2f4722b-fb4a-0410-96a1-771b68bbab1a>
2009-04-15 06:21:56 +0000

    added functionality that allows use data from statistics database query result in graph

--- raven-core/src/test/java/org/raven/graph/impl/SdbQueryResultDataDefTest.java
+++ raven-core/src/test/java/org/raven/graph/impl/SdbQueryResultDataDefTest.java
@@ -17,17 +17,69 @@
 
 package org.raven.graph.impl;
 
+import java.util.Arrays;
+import java.util.Map;
+import org.jrobin.data.Plottable;
 import org.junit.Test;
+import org.raven.PushOnDemandDataSource;
+import org.raven.RavenCoreTestCase;
+import org.raven.graph.DataDefException;
+import org.raven.statdb.impl.KeyValuesImpl;
+import org.raven.statdb.impl.QueryResultImpl;
+import org.raven.statdb.impl.SdbQueryResultNode;
+import org.raven.statdb.impl.StatisticsValuesImpl;
+import org.raven.statdb.query.KeyValues;
+import org.raven.tree.NodeAttribute;
 
 /**
  *
  * @author Mikhail Titov
  */
-public class SdbQueryResultDataDefTest
+public class SdbQueryResultDataDefTest extends RavenCoreTestCase
 {
     @Test
-    public void test()
+    public void test() throws DataDefException
     {
+        PushOnDemandDataSource ds = new PushOnDemandDataSource();
+        ds.setName("ds");
+        tree.getRootNode().addAndSaveChildren(ds);
+        assertTrue(ds.start());
 
+        KeyValuesImpl keyValues1 = new KeyValuesImpl("/test/");
+        KeyValuesImpl keyValues2 = new KeyValuesImpl("/test1/");
+        keyValues2.addStatisticsValues(new StatisticsValuesImpl("s1", new double[]{10, 20}));
+        keyValues2.addStatisticsValues(new StatisticsValuesImpl("s2", new double[]{100, 200}));
+        QueryResultImpl res = new QueryResultImpl(
+                Arrays.asList((KeyValues)keyValues1, (KeyValues)keyValues2));
+        res.setTimestamps(new long[]{0, 100});
+
+        ds.addDataPortion(res);
+
+        SdbQueryResultDataDef def = new SdbQueryResultDataDef();
+        def.setName("def");
+        tree.getRootNode().addAndSaveChildren(def);
+        def.setDataSource(ds);
+        def.setKey("/test1/");
+        def.setStatisticsName("s1");
+        assertTrue(def.start());
+
+        Plottable data = def.getData(0l, 100l);
+        assertNotNull(data);
+
+        assertEquals(10., data.getValue(0l), 0.);
+        assertEquals(20., data.getValue(100l), 0.);
+
+        Map<String, NodeAttribute> attrs = ds.getLastSessionAttributes();
+        assertNotNull(attrs);
+        checkTimeAttr(SdbQueryResultNode.STARTTIME_SESSION_ATTRIBUTE, "0L", attrs);
+        checkTimeAttr(SdbQueryResultNode.ENDTIME_SESSION_ATTRIBUTE, "100L", attrs);
+    }
+
+    private void checkTimeAttr(String attrName, String attrValue, Map<String, NodeAttribute> attrs)
+    {
+        NodeAttribute attr = attrs.get(attrName);
+        assertNotNull(attr);
+        assertEquals(String.class, attr.getType());
+        assertEquals(attrValue, attr.getValue());
     }
 }
