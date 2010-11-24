37d742a3602946d4a2e7b7945bfc17628cdc5b56
mikhail1207 <mikhail1207@d2f4722b-fb4a-0410-96a1-771b68bbab1a>
2010-11-24 14:53:19 +0000

    working on refactoring in the auth layer

---
+++ raven-core/src/main/java/org/raven/auth/impl/NodeAccessServiceImpl.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright 2010 Mikhail Titov.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.raven.auth.impl;
+
+import org.raven.auth.NodeAccessService;
+import org.raven.auth.UserContext;
+import org.raven.conf.Configurator;
+import org.raven.tree.Node;
+import org.raven.tree.Tree;
+
+/**
+ *
+ * @author Mikhail Titov
+ */
+public class NodeAccessServiceImpl implements NodeAccessService
+{
+    private final Tree tree;
+    private final Configurator configurator;
+
+    public NodeAccessServiceImpl(Tree tree, Configurator configurator)
+    {
+        this.tree = tree;
+        this.configurator = configurator;
+    }
+
+    public int getAccessForNode(Node node, UserContext context)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
