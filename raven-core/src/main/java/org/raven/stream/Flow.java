1c89ab4e602ff4afa19787ff6833af77b2acac3e
tim1 <tim1@192.168.1.9>
2016-04-11 08:29:16 +0400

    working on FileWriterDP

---
+++ raven-core/src/main/java/org/raven/stream/Flow.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 Mikhail Titov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.raven.stream;
+
+import org.raven.dp.RavenFuture;
+
+/**
+ *
+ * @author Mikhail Titov
+ */
+public interface Flow<A, B> {
+    public final static int PROCESSED = 0;
+    public final static int INPUT_NOT_CONSUMED = 1;
+    public final static int OUTPUT_NOT_READY = 2;
+    public final static int COMPUTING = 4;
+    
+    public interface MapResult<B> {
+        public void setValue(B value);
+        public void getValue(B vaule);
+        public void setFuture(RavenFuture<Integer, Throwable> future);
+    }
+    
+    public int map(A input, MapResult<B> result);
+    public void onComplete();
+    public void onError();
+}
---
+++ raven-core/src/main/java/org/raven/stream/Sink.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Mikhail Titov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.raven.stream;
+
+import org.raven.dp.RavenFuture;
+
+/**
+ *
+ * @author Mikhail Titov
+ */
+public interface Sink<B> {
+    public enum OperationResult {DONE, INPUT_NOT_CONSUMED, COMPUTING};
+    
+    public interface Result {
+        public OperationResult getOperationResult();
+        public void setOperationResult(OperationResult operationResult);
+        public RavenFuture<OperationResult, Throwable> getResultFuture();
+        public void setResultFuture(RavenFuture<OperationResult, Throwable> future);
+    };
+    
+    public void onNext(B message, OperationResult result);
+    public void onComplete();
+    public void onError();
+}
