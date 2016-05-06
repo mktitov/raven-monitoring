2610e8c86204be7ebccecbfc3fc23e522a0acc00
tim1 <tim1@192.168.1.8>
2016-05-06 08:23:57 +0400

    working on reactive stream functionality

--- raven-core/src/main/java/org/raven/net/http/server/HttpServerHandler.java
+++ raven-core/src/main/java/org/raven/net/http/server/HttpServerHandler.java
@@ -19,10 +19,17 @@ import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.util.ReferenceCountUtil;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import org.raven.net.NetworkResponseService;
 import org.raven.sched.ExecutorService;
 import org.raven.tree.Node;
 import org.raven.tree.impl.LoggerHelper;
@@ -36,14 +43,19 @@ public class HttpServerHandler extends ChannelDuplexHandler {
     private final Node owner;
     private final AtomicLong connectionCounter;
     private final AtomicLong requestCounter;
+    private final NetworkResponseService responseService;
+    
     private LoggerHelper logger;
     private RRController rrController;
 
-    public HttpServerHandler(ExecutorService executor, Node owner, AtomicLong connectionCounter, AtomicLong requestCounter) {
+    public HttpServerHandler(ExecutorService executor, Node owner, AtomicLong connectionCounter, AtomicLong requestCounter, 
+            NetworkResponseService responseService) 
+    {
         this.executor = executor;
         this.owner = owner;
         this.connectionCounter = connectionCounter;
         this.requestCounter = requestCounter;
+        this.responseService = responseService;
     }
 
     @Override
@@ -89,7 +101,43 @@ public class HttpServerHandler extends ChannelDuplexHandler {
     }
     
     private void processHttpRequest(HttpRequest request, ChannelHandlerContext ctx) {
+        final long requestNum = requestCounter.incrementAndGet();
+        if (logger.isDebugEnabled())
+            logger.debug("Received new request #"+requestNum);
+        //экстрактим параметры
+        Map<String, Object> params = decodeParams(request);
+        //экстрактим заголовки
         
+        //нужно создать Raven Request
+        
+        //нужно создать ResponseContext
+        
+        //проверяем аутентификацию
+        
+        //добавляем запись в audit
+        
+        //создаем RRController
+    }
+    
+    private static Map<String, Object> decodeParams(final HttpRequest request) {
+        final Map<String, List<String>> params = new QueryStringDecoder(request.getUri()).parameters();
+        final Map<String, Object> requestParams = new HashMap<>();
+        if (params!=null && !params.isEmpty()) {
+            List<String> vals;
+            for (Map.Entry<String, List<String>> param: params.entrySet()) {            
+                vals = param.getValue();
+                if (vals!=null && !vals.isEmpty()) 
+                    requestParams.put(param.getKey(), vals.get(0)); //берем только первой значение параметра, хоть это не совсем корректно
+            }
+        }
+        return requestParams;
+    }
+    
+    private static Map<String, Object> decodeHeaders(final HttpRequest request) {
+        final HttpHeaders httpHeaders = request.headers();
+        Map<String, Object> headers = new HashMap<>();
+        if (!httpHeaders.isEmpty())
+            
     }
 
     private void processHttpRequestContent(HttpContent content) {
